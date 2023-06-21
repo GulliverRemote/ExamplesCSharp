69f58a44b238f012240c4302e3253c0fe84427e9
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-21 22:08:33 +0300

    add: Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "gulliver")
+{
+    Console.WriteLine("Здарова, ОТЕЦ!");
+}
+else
+{
+    Console.WriteLine("Хто ты, ГОЛОВА?");
+}
