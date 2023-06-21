ffe0d41b0728687b39cdaccb3e08866aa2b061ad
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-21 21:55:46 +0300

    feat: add random in Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3 , numberB = 5;
+﻿int numberA = new Random().Next(1,10) , // 1 2 3 ... 9
+    numberB = new Random().Next(1,10);
 int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(result);
