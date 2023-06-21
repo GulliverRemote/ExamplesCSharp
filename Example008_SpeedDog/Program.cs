cf5a20c867a46426a9126d7f2c346230aa46aa15
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-21 23:11:08 +0300

    add: Example 008 SpeedDog feat: input information

---
+++ Example008_SpeedDog/Program.cs
@@ -0,0 +1,9 @@
+﻿double v1dr = new Random().Next(0, 6),
+       v2dr = new Random().Next(0, 6),
+       vDog = new Random().Next(0, 7),
+       smezh = new Random().Next(5, 1000);
+
+Console.WriteLine($"Скорость 1 друга = {v1dr}");
+Console.WriteLine($"Скорость 2 друга = {v2dr}");
+Console.WriteLine($"Скорость собаки = {vDog}");
+Console.WriteLine($"Расстояние между друзьями = {smezh}");
