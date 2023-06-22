72f50330e272f4ab1516c8188712d708b9b09891
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-22 20:10:28 +0300

    feat: add correction output

--- Example008_SpeedDog/Program.cs
+++ Example008_SpeedDog/Program.cs
@@ -1,17 +1,10 @@
 ﻿double v1dr = new Random().Next(1, 6),
        v2dr = new Random().Next(1, 6),
        smezh = new Random().Next(5, 50),
-       
+       vDog = new Random().Next(1, 7),
        t = 0;
 int count = 0;
 bool friend = false;
-
-int max = (int)v1dr;
-
-if (max < v2dr)
-    max = (int)v2dr;
-double vDog = new Random().Next(max, 7);
-
 
 Console.WriteLine($"Скорость 1 друга = {v1dr}");
 Console.WriteLine($"Скорость 2 друга = {v2dr}");
@@ -19,22 +12,40 @@ Console.WriteLine($"Скорость собаки = {vDog}");
 Console.WriteLine($"Расстояние между друзьями = {smezh}");
 
 
-while (smezh >= 3)
+if (vDog <= v1dr)
 {
-    if (friend == false)
-    {
-        t = smezh / (vDog + v2dr);
-        friend = true;
-    }
-    else
-    {
-        t = smezh / (vDog + v1dr);
-        friend = false;
-    }
-    smezh = (v1dr + v2dr) * t;
-    smezh = Math.Round(smezh, 4);
-    Console.WriteLine(smezh);
-    count++;
+    Console.WriteLine("Собака слишком медленная чтобы бегать между друзьями");
+    Environment.Exit(0);
 }
+else if (vDog < v2dr)
+{
+    Console.WriteLine("Собака добежит до 2 друга и не догонит его");
+    Environment.Exit(0);
+}
+else if (vDog == v2dr)
+{
+    Console.WriteLine("Собака добежит до 2 друга и побежит на равне с ним");
+    Environment.Exit(0);
+}
+else
+{
+    while (smezh >= 3)
+    {
+        if (friend == false)
+        {
+            t = smezh / (vDog + v2dr);
+            friend = true;
+        }
+        else
+        {
+            t = smezh / (vDog + v1dr);
+            friend = false;
+        }
+        smezh = (v1dr + v2dr) * t;
+        smezh = Math.Round(smezh, 4);
+        Console.WriteLine(smezh);
+        count++;
+    }
 
-Console.WriteLine($"Собака пробежала {count} раз");
+    Console.WriteLine($"Собака пробежала {count} раз");
+}
